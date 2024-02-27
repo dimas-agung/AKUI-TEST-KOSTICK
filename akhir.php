4007d94955a29a5b8760407da93b6c36e911d326
ilhammy <ilhamze231@gmail.com>
2024-02-27 15:38:14 +0700

    update halaman akhir

--- akhir.php
+++ akhir.php
@@ -4,21 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Terima Kasih</title>
-    <meta name="description" content="Tes papikostick">
-    <meta name="author" content="Clarymond Simbolon" />
     <link rel="shortcut icon" type="image/png" href="logo2.svg" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <style>
+        body {
+            background-color: #f0f0f0;
+        }
+
+        .container {
+            margin-top: 100px;
+            background-color: #d4edda;
+            /* Bootstrap Success Color */
+            padding: 30px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            /* Adding Box Shadow */
+        }
+    </style>
 </head>
 <body>
-<div class="container pt-3">
-      <div class="card border-top border-warning">
-        <div class="card-body">
-          <h5 class="card-title">Terima Kasih telah mengisi Tes</h5>
-
-          <p class="card-text">Jawaban tes anda telah terkirim <br> silahkan menunggu informasi HR selanjutnya</p>
-          
-        </div>
-      </div>
-    </div>
+  <div class="container text-center">
+        <h1 class="mb-4">Terima Kasih telah mengerjakan Tes Papi Kostick</h1>
+        <p class="lead">Jawaban tes anda telah terkirim <br> silahkan menunggu informasi HR selanjutnya</p>
+  </div>
 </body>
 </html>
